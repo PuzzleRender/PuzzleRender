828f978c3aac0167393b2dcc364a03796dea8c10
Dohou Daniel Favour <dohoudanielfavour@gmail.com>
2024-08-19 19:07:11 +0100

    Update models.py

--- backend/puzzlerender/prenderapp/models.py
+++ backend/puzzlerender/prenderapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+import uuid
+"""
+The database models app (ORM)
+"""
 
-# Create your models here.
+
+class Puzzle(models.Model):
+  """
+  The Puzzle table
+  """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    pdf_file = models.FileField(upload_to='puzzles/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+      return self.title
+
+
+class Clue(models.Model):
+  """
+  The Class table
+  """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    puzzle = models.ForeignKey(Puzzle, related_name='clues', on_delete=models.CASCADE)
+    clue_text = models.TextField()
+    pdf_file = models.FileField(upload_to='clues/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Clue for {self.puzzle.title}"
