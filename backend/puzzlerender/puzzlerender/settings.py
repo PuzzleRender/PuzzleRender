46e5236d17e3278256fa735b6faa772e95add0a0
Dohou Daniel Favour <dohoudanielfavour@gmail.com>
2024-08-19 19:41:40 +0100

    Add remote MySQL database details

--- backend/puzzlerender/puzzlerender/settings.py
+++ backend/puzzlerender/puzzlerender/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'puzzlerender.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB'), # Name of the database
+        'USER': os.getenv('DB_USER'), # User
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_IP'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
