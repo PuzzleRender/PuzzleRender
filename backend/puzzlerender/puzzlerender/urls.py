898623417f7ce3fa126c3397e5e40899615066c5
Olusegun .O. ADELEKE <oadeleke123@gmail.com>
2024-08-19 22:07:17 +0100

    Add: home url

--- backend/puzzlerender/puzzlerender/urls.py
+++ backend/puzzlerender/puzzlerender/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/auth/', include('dj_rest_auth.urls')),  # Authentication endpoints (login, logout, etc.)
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),  # Registration endpoints
